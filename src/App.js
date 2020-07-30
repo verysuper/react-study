253633fc479c0750d21e305487f3384f61af8b00
熊大 <brown@brown.com>
2020-07-30 23:24:48 +0800

    插槽

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ParentCom extends React.Component{
+  render() {
+      return (
+          <div>
+              <ChildCom>
+                  <h1 data-position="header">header</h1>
+                  <h1 data-position="main">main</h1>
+                  <h1 data-position="footer">footer</h1>
+              </ChildCom>
+          </div>
+      )
+  }
+}
+
+class ChildCom extends React.Component{
+    render() {
+        let header,main,footer
+        this.props.children.forEach((item)=>{
+            switch (this.props['data-position']) {
+                case "header":
+                    header = item
+                    break
+                case "main":
+                    main = item
+                    break
+                case "footer":
+                    footer = item
+            }
+        })
+        return (
+            <div>
+                {header}
+                {main}
+                {footer}
+            </div>
+        )
+    }
+}
+
+class App extends React.Component{
+    render() {
+        return (
+            <ParentCom />
+        )
+    }
 }
 
 export default App;
