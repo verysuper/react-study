7047fc3c90198fef8bedc883291b8271e7813af6
熊大 <brown@brown.com>
2020-08-02 21:00:43 +0800

    ok

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class AB extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            ABData : null
+        }
+    }
+
+    handle=()=>{
+        if(this.state.ABData!==null){
+            console.log("aaa")
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                {this.handle()}
+                <h1>A2來的: {this.state.ABData}</h1>
+                <A1 setABData={this.setABData} />
+            </div>
+        )
+    }
+
+    setABData=(data)=>{
+        this.setState({
+            ABData:data
+        })
+    }
+}
+
+class A1 extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <A2 setABData={(data)=>this.props.setABData(data)} />
+            </div>
+        )
+    }
+}
+
+class A2 extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            msg:"hello world"
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.sendData}>傳遞數據</button>
+            </div>
+        )
+    }
+    sendData = ()=>{
+        console.log("bbb")
+        this.props.setABData(this.state.msg)
+    }
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <AB />,
+    document.querySelector('#root')
+)
